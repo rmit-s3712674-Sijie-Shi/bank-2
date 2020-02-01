433d43f71a77717ce48686ab8a418c6329906131
rmit-s3669556-qing-qin <48906998+rmit-qingqin@users.noreply.github.com>
2020-02-02 00:23:38 +1100

    Add files via upload

---
+++ Controllers/BillPaysController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using bank.Data;
+using bank.Models;
+
+namespace bank.Controllers
+{
+    public class BillPaysController : Controller
+    {
+        private readonly bankContext _context;
+
+        public BillPaysController(bankContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BillPays
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BillPay.ToListAsync());
+        }
+
+        // GET: BillPays/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var billPay = await _context.BillPay
+                .FirstOrDefaultAsync(m => m.BillPayID == id);
+            if (billPay == null)
+            {
+                return NotFound();
+            }
+
+            return View(billPay);
+        }
+
+        // GET: BillPays/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BillPays/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BillPayID,Period,AccountNumber1,PayeeID,Amount,ScheduleDate,BillPayTimeUtc")] BillPay billPay)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(billPay);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(billPay);
+        }
+
+        // GET: BillPays/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var billPay = await _context.BillPay.FindAsync(id);
+            if (billPay == null)
+            {
+                return NotFound();
+            }
+            return View(billPay);
+        }
+
+        // POST: BillPays/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BillPayID,Period,AccountNumber1,PayeeID,Amount,ScheduleDate,BillPayTimeUtc")] BillPay billPay)
+        {
+            if (id != billPay.BillPayID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(billPay);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BillPayExists(billPay.BillPayID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(billPay);
+        }
+
+        // GET: BillPays/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var billPay = await _context.BillPay
+                .FirstOrDefaultAsync(m => m.BillPayID == id);
+            if (billPay == null)
+            {
+                return NotFound();
+            }
+
+            return View(billPay);
+        }
+
+        // POST: BillPays/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var billPay = await _context.BillPay.FindAsync(id);
+            _context.BillPay.Remove(billPay);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BillPayExists(int id)
+        {
+            return _context.BillPay.Any(e => e.BillPayID == id);
+        }
+    }
+}
---
+++ Controllers/DisplayController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using bank.Data;
+using bank.Models;
+using bank.Utilities;
+using bankWithLogin.Attributes;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace bank.Controllers
+{
+    public class DisplayController : Controller
+    {
+        private readonly bankContext _context;
+
+        private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+
+        public DisplayController(bankContext context) => _context = context;
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> TransactionDisplay(int id) => View(await _context.Accounts.FindAsync(id));
+        [HttpPost]
+        public async Task<IActionResult> TransactionDisplay()
+        {
+            var customer = await _context.Customers.FindAsync(CustomerID);
+            return View("TransactionDisplay");
+        }
+        public async Task<IActionResult> BillPayDisplay(int id) => View(await _context.Accounts.FindAsync(id));
+        [HttpPost]
+        public async Task<IActionResult> BillPayDisplay()
+        {
+            var customer = await _context.Customers.FindAsync(CustomerID);
+            return View("BillPayDisplay");
+        }
+    }
+}
